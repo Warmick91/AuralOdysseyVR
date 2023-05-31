befab661f75c2bf45796eac5e31699da7d63738e
Krzysztof Mickiewicz <107584697+Warmick91@users.noreply.github.com>
2023-05-31 12:35:26 +0200

    Set up pedestal data for Antiquity, MiddleAges, and Classicism

--- Assets/Scripts/Custom Scripts/MusicControlPanel.cs
+++ Assets/Scripts/Custom Scripts/MusicControlPanel.cs
@@ -129,5 +129,7 @@ public class MusicControlPanel : MonoBehaviour
         {
             mainAudioSource.clip = periodTracks[currentTrack];
         }
+
+        dropdownTracks.value = currentTrack;
     }
 }
