ce40775ccfa50640741c3e4900dfcf8633350f0e
Krzysztof Mickiewicz <107584697+Warmick91@users.noreply.github.com>
2023-06-01 10:10:22 +0200

    All pedestals data added

--- Assets/Scriptable Objects/Scriptable Images/Placeholder_Image01.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Placeholder_Image01", menuName = "Scriptable Objects/Images/Placeholder_Image01")]
-public class Placeholder_Image01 : ImageData
-{
-
-}
--- Assets/Scriptable Objects/Scriptable Images/Placeholder_Image02.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Placeholder_Image02", menuName = "Scriptable Objects/Images/Placeholder_Image02")]
-public class Placeholder_Image02 : ImageData
-{
-
-}
